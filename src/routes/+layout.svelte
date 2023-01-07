fe84fd5cf6e2bc64b1fbff11d78481422314c348
Tyler Losinski <tyler.a.losinski@gmail.com>
2023-01-07 17:15:06 -0600

    Gave up again :(

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,3 @@
-<script>
-  import { webVitals } from '$lib/vitals'
-  import { browser } from '$app/env'
-  import { page } from '$app/stores'
-  import '../app.css'
-  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID
-  $: if (browser && analyticsId) {
-    webVitals({
-      path: $page.url.pathname,
-      params: $page.params,
-      analyticsId
-    })
-  }
-</script>
+
 
 <slot />
